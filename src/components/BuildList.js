d38b648eb563b6d471d72ba60f52e200d8b9bebf
cpage2 <mcpage25@gmail.com>
2023-11-04 13:15:22 -0500

    update buildList

--- src/components/BuildList.js
+++ src/components/BuildList.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Checkbox from "./Checkbox.js";
 import EditButton from "./EditButton.js";
+import { useState } from "react";
 
 
 function BuildList({inputData}){
+    const[listItem, setListItem] = useState("");
 
     function listCreation(){
+        //take checkbox, input, and edit button and create list item with unique id
+        /**
+         * create label
+         * create list item (document.createElement('li'))
+         */
         
+
+
     }
 
     return(
         <>
-            <p>{inputData}</p>
+            <ul>
+                {listCreation()}
+            </ul>
         
         
         </>
