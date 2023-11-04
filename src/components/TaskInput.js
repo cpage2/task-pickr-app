8fb22d1e62c8d5aa85a00da6f8ccd370f15e86d7
cpage2 <mcpage25@gmail.com>
2023-11-04 12:38:44 -0500

    added input box

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -1,5 +1,38 @@
+import React from "react";
+import { useState } from "react";
+
 function TaskInput(){
+    //input bar --> sends value to var to  Buildlist
+   
+    const [input, setInput] = useState("");
+    
+    let inputValue = "";
+    function submitInput(e){
+        
+        e.preventDefault(); //stops page from reloading
+        inputValue = input;
+        setInput(""); //reset input value
+        // console.log(inputValue);
+    }
+
+
+
+
+    return(
+        <>
+        <form onSubmit={submitInput}>
+            <input type="text" class="form-control" placeholder="Enter task" value={input} onChange={(e) => setInput(e.target.value)} />
+        </form>
+    
+        </>
+
+
+
+    );
+   
 
 }
+
+
 
 export default TaskInput;
